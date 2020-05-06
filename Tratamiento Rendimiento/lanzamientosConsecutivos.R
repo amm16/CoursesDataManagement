b58cde7056e6729bb2a7c783f993ccce977c887c
Allan J. Martinez <jafectnez@gmail.com>
2020-05-05 23:47:44 -0600

    comienzo de las funciones para validar

--- Tratamiento Rendimiento/lanzamientosConsecutivos.R
+++ Tratamiento Rendimiento/lanzamientosConsecutivos.R
@@ -1,28 +1,33 @@
-#Libraries
-library(dplyr)
+#setwd("C:/Users/Asus ROG i7/Desktop/UNAH/Seminario/Repo Seminario/CoursesDataManagement")
+
+lanzamientoMasBajo <- function(registros) {
+  
+  
+}
+
+validarOrden <- function(registro, datos, output) {
+  #El orden de los lanzamientos es:
+  #Winter -> Spring -> Summer -> Fall
+  # orden <- data.frame(Lanzamiento = c('Winter', 'Spring', 'Summer', 'Fall', 'Winter', 'Spring', 'Fall'), 
+  #                     Año = c('2018', '2018', '2018', '2018', '2019', '2019', '2019'))
+  
+
+  infoPersona <- datos[which(datos$correo == registro[4]),]
+  lanzamientosRegistrados <- data.frame(lanzamientos = infoPersona$lanzamiento, año = infoPersona$aÃ.o)
+  
+  lanzamientoMasBajo()
+  
+  print(lanzamientosRegistrados)
+  
+  
+  # print(paste(year, launch, email, sep=","))
+  # cat(paste(year, launch, email, sep=","), file=output, append = T, fill = T)
+}
 
 #Obteniendo el CSV
-coursesData <- read.csv('Tratamiento Valores Nulos/Tratamiento-limpieza-datos.csv')
+coursesData <- read.csv('analisis_descriptivo/courses_data_analisis_categorias_nivel_estado.csv')
 
-#Se obtiene el data.frame de los correos adjuntos con el a??o y los lanzamientos
-correos <- data.frame(Correos = coursesData$correo, Anios = coursesData$a??o, Lanzamientos = coursesData$lanzamiento)
+#Se obtiene el data.frame de los correos adjuntos con el año y los lanzamientos
+correos <- data.frame(Correos = coursesData$correo, Anios = coursesData$aÃ.o, Lanzamientos = coursesData$lanzamiento)
 
-#Se hace un conteo de cuantas veces aparece el correo en los datos
-conteo <- aggregate(correos$Correos, correos, length)
-colnames(conteo)[4]<-"Repeticiones"
-
-#El orden de los lanzamientos seran tratados:
-#Spring -> Summer -> Fall -> Winter
-
-#Se valida si el correo tiene menos de 2 repeticiones pero son
-#distintas de Fall y Winter de 2019 (por ser los ultimos y no sabemos si continua en 2020)
-
-conteo[(conteo$Anios == '2019') && ((conteo$Lanzamientos %in% c('Winter')) && (conteo$Lanzamientos%in% c('Fall'))) && (conteo$Repeticiones < 2), 'Consecutivo'] <- ''
-conteo[(conteo$Anios == '2019') && ((conteo$Lanzamientos %in% c('Winter')) && (conteo$Lanzamientos%in% c('Fall'))) && (conteo$Repeticiones < 2), 'Consecutivo'] <- 'faltante'
-conteo[(conteo$Anios != '2019') && (conteo$Repeticiones < 2), 'Consecutivo'] <- 'faltante'
-conteo[(conteo$Anios != '2019') && (conteo$Repeticiones < 2), 'Consecutivo'] <- 'faltante'
-
-conteo[conteo$Repeticiones >= 2, 'Consecutivo'] <- 'recurrente'
-
-
-summary(as.factor(conteo$Consecutivo))
+apply(coursesData, 1, validarOrden, datos = coursesData, output = 'estudiantes-consecutivos.csv')
