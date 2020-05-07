9c798f4674a69d4f36d9ff398379d200dfa2a10c
Allan J. Martinez <jafectnez@gmail.com>
2020-05-06 23:49:49 -0600

    Lanzamientos consecutivos obtenidos

--- Tratamiento Rendimiento/lanzamientosConsecutivos.R
+++ Tratamiento Rendimiento/lanzamientosConsecutivos.R
@@ -1,33 +1,108 @@
 #setwd("C:/Users/Asus ROG i7/Desktop/UNAH/Seminario/Repo Seminario/CoursesDataManagement")
 
-lanzamientoMasBajo <- function(registros) {
-  
-  
-}
-
 validarOrden <- function(registro, datos, output) {
-  #El orden de los lanzamientos es:
-  #Winter -> Spring -> Summer -> Fall
-  # orden <- data.frame(Lanzamiento = c('Winter', 'Spring', 'Summer', 'Fall', 'Winter', 'Spring', 'Fall'), 
-  #                     Año = c('2018', '2018', '2018', '2018', '2019', '2019', '2019'))
+  # El orden de los lanzamientos es:
+  # Winter -> Spring -> Summer -> Fall
+  # Excepto en 2019 que no hubo lanzamiento Summer
   
-
   infoPersona <- datos[which(datos$correo == registro[4]),]
   lanzamientosRegistrados <- data.frame(lanzamientos = infoPersona$lanzamiento, año = infoPersona$aÃ.o)
   
-  lanzamientoMasBajo()
   
-  print(lanzamientosRegistrados)
+  if (c('2018') %in% lanzamientosRegistrados$año) {
+    if (c('Winter') %in% lanzamientosRegistrados$lanzamientos) {
+      menor <- data.frame(lanzamiento = c('Winter'), año = c('2018'))
+    } else if (c('Spring') %in% lanzamientosRegistrados$lanzamientos) {
+      menor <- data.frame(lanzamiento = c('Spring'), año = c('2018'))
+    } else if (c('Summer') %in% lanzamientosRegistrados$lanzamientos) {
+      menor <- data.frame(lanzamiento = c('Summer'), año = c('2018'))
+    } else if (c('Fall') %in% lanzamientosRegistrados$lanzamientos) {
+      menor <- data.frame(lanzamiento = c('Fall'), año = c('2018'))
+    }
+  }else {
+    if (c('Winter') %in% lanzamientosRegistrados$lanzamientos) {
+      menor <- data.frame(lanzamiento = c('Winter'), año = c('2019'))
+    } else if (c('Spring') %in% lanzamientosRegistrados$lanzamientos) {
+      menor <- data.frame(lanzamiento = c('Spring'), año = c('2019'))
+    } else if (c('Fall') %in% lanzamientosRegistrados$lanzamientos) {
+      menor <- data.frame(lanzamiento = c('Fall'), año = c('2019'))
+    }
+  }
+  
+  resultado = FALSE
+  count = 1
+  año = as.character(menor$año)
+  siguiente = as.character(menor$lanzamiento) #La variable siguiente se usará para tener el lanzamiento inicial y luego validar el que le sigue
+  orden = siguiente
+  
+  #Header del archivo resultante
+  cat(paste(c('correo'), c('resultado'), c('lanzamientos'), sep=","), file=output, append = T, fill = T)
+  # Mientras no se obtenga un resultado
+  while (resultado == FALSE) {
+    #Se evalua qué año es el menor
+    if (año == c('2018')) {
+      #Si el lanzamiento que se esta evaluando es "fall" entonces se llego al final del año y se continua evaluando el otro
+      if (siguiente == c('fall')) {
+        año = c('2019')
+      }
+      
+      #Se obtiene cual es el siguiente lanzamiento a ser evaluado
+      siguiente <- switch(siguiente, "Winter" = "Spring", "Spring" = "Summer", "Summer" = "Fall", "Fall" = "Winter")
+      
+      #Si el lanzamiento siguiente no aparece entre los registrados, se termina el ciclo
+      if (!(siguiente %in% lanzamientosRegistrados$lanzamientos)) {
+        resultado = c('Faltante')
+        break
+      }
+      #Si el lanzamiento estaba entre los registrados el ciclo continua
+      count = count + 1
+      orden = paste(orden, paste(siguiente, año, sep=""), sep="-")
+      
+      #Si tiene al menos 3 lanzamientos consecutivos se le considera un estudiante recurrente
+      if (count >= 3) {
+        resultado = c('Recurrente')
+        break
+      }
+    } else {
+      
+      #Si se llego al final de los lanzamientos se cataloga al estudiante como "recurrente"
+      if (siguiente == c('next')) {
+        resultado = c('Recurrente')
+        break
+      }
+      
+      #Se obtiene cual es el siguiente lanzamiento a ser evaluado
+      siguiente <- switch(siguiente, "Winter" = "Spring", "Spring" = "Fall", "Fall" = "next")
+      
+      #Si el lanzamiento siguiente no aparece entre los registrados, se termina el ciclo
+      if (!(siguiente %in% lanzamientosRegistrados$lanzamientos)) {
+        resultado = c('Faltante')
+        break
+      }
+      
+      #Si el lanzamiento estaba entre los registrados el ciclo continua
+      count = count + 1
+      orden = paste(orden, paste(siguiente, año, sep=""), sep="-")
+      
+      #Si tiene al menos 3 lanzamientos consecutivos se le considera un estudiante recurrente
+      if (count >= 3) {
+        resultado = c('Recurrente')
+        break
+      }
+    }
+  }
   
   
-  # print(paste(year, launch, email, sep=","))
-  # cat(paste(year, launch, email, sep=","), file=output, append = T, fill = T)
+  cat(paste(registro[4], resultado, orden, sep=","), file=output, append = T, fill = T)
 }
 
 #Obteniendo el CSV
 coursesData <- read.csv('analisis_descriptivo/courses_data_analisis_categorias_nivel_estado.csv')
 
-#Se obtiene el data.frame de los correos adjuntos con el año y los lanzamientos
-correos <- data.frame(Correos = coursesData$correo, Anios = coursesData$aÃ.o, Lanzamientos = coursesData$lanzamiento)
+apply(coursesData, 1, validarOrden, datos = coursesData, output = 'Tratamiento Rendimiento/estudiantes-consecutivos.csv')
 
-apply(coursesData, 1, validarOrden, datos = coursesData, output = 'estudiantes-consecutivos.csv')
+#Buscar la informacion de un correo en especifico
+infoPersona <- coursesData[which(coursesData$correo == c('CIBERTECMO@5130MOcibertec.pe')),]
+lanzamientosRegistrados <- data.frame(lanzamientos = infoPersona$lanzamiento, año = infoPersona$aÃ.o)
+print(lanzamientosRegistrados)
+
